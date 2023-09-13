fea19a554dc2dbf866d4145ea36ffa4234b6f970
Abdeljalil37 <widadpop@gmail.com>
2023-09-13 22:46:18 +0100

    I will do it later

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_bytes(PyObject *p)
+{
+
+}
+
+/**
+ * print_python_list - Prints list information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_list(PyObject *p)
+{
+
+}
