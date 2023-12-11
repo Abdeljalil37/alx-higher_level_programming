c175890f14f5073f9bd84fde536e20893b6f59f7
Abdeljalil37 <widadpop@gmail.com>
2023-12-11 18:03:29 +0100

    try fix error

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 
 console.log(process.argv.length);
 if (process.argv.length === 2) {
-  console.log('undefined is undefined');
+  console.log(process.argv[4], 'is', process.argv[4]);
 } else if (process.argv.length === 3) {
   console.log(process.argv[2], 'is', process.argv[3]);
 } else if (process.argv.length === 4) {
