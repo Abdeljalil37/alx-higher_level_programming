1dce0d97a3b1d4d55547c2fb2a3616c0ca23c63e
Abdeljalil37 <widadpop@gmail.com>
2023-12-12 02:08:00 +0100

    last try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
 const arg = process.argv.length;
-let biggest = 0;
-let max = 0;
 
 if (arg <= 3) {
   console.log(0);
 } else {
-  for (let i = 1; i <= 2; i++) {
-    if (parseInt(process.argv[i + 1]) > max) {
-      biggest = parseInt(process.argv[i + 1]);
-    }
-    max = biggest;
+  const args = [];
+  for (let i = 2; i < arg; i++) {
+    args.push(process.argv[i]);
   }
-  console.log(biggest);
+  args.sort();
+  console.log(args[args.length - 2]);
 }
