4dc15e334d331c541b64d85f448c468745725415
Abdeljalil37 <widadpop@gmail.com>
2023-12-12 02:03:12 +0100

    fix error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ let max = 0;
 if (arg <= 3) {
   console.log('0');
 } else {
-  for (let i = 1; i <= arg; i++) {
+  for (let i = 1; i <= 3; i++) {
     if (parseInt(process.argv[i + 1]) > max) {
       biggest = parseInt(process.argv[i + 1]);
     }
