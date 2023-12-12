939b9f3257777ec49fe5b956017d79340d6239eb
Abdeljalil37 <widadpop@gmail.com>
2023-12-12 13:33:37 +0100

    trying fix error

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
-    if (parseInt(w) <= 0 || parseInt(h) <= 0) {
-      return Rectangle;
+    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
+
 module.exports = Rectangle;
