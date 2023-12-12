d8eb2e02093e5998247445a85c384b2901bd32a8
Abdeljalil37 <widadpop@gmail.com>
2023-12-12 01:30:40 +0100

    error fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ let biggest = 0;
 let max = 0;
 
 if (arg <= 3) {
-  console.log('1');
+  console.log('0');
 } else {
   for (let i = 1; i <= arg; i++) {
     if (parseInt(process.argv[i + 1]) > max) {
