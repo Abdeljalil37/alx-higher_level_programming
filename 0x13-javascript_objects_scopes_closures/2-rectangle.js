93bb68552b041d447e15a9976800f058a70904b3
Abdeljalil37 <widadpop@gmail.com>
2023-12-12 13:21:07 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor (w, h) {
+    if (parseInt(w) <= 0 || parseInt(h) <= 0) {
+      return Rectangle;
+    }
     this.width = w;
     this.height = h;
   }
-  
 }
 module.exports = Rectangle;
