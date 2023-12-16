7426b03a6073400f2602510a911043f09bccc14c
Abdeljalil37 <widadpop@gmail.com>
2023-12-16 23:56:03 +0100

    error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,12 +6,13 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    curs = db.cursor()
+
+    con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    curs = con.cursor()
     curs.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
     rows = curs.fetchall()
     for row in rows:
         print(row)
     curs.close()
-    db.close()
+    con.close()
