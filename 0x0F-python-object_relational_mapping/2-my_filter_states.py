223e99b3d7f194f0c87143e6d56ab9601286cfb4
Abdeljalil37 <widadpop@gmail.com>
2023-12-16 23:54:55 +0100

    error task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    curs.execute("select * from states where name = '{}'".format(sys.argv[4],))
+    curs.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
     rows = curs.fetchall()
     for row in rows:
         print(row)
