701f4d8536b98e745826938de4668f8402c4b54c
Abdeljalil37 <widadpop@gmail.com>
2024-01-06 10:16:36 +0100

    try fix error task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the passed URL, and displays the body of the response
-curl --data "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
+curl --data "email=test@gmail.com&subject=I will always be here for PLD" -sX POST "$1"
