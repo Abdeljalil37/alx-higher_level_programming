22a398eac1e50beeb6f68c63207a8cf5a8518fda
Abdeljalil37 <widadpop@gmail.com>
2024-01-07 23:58:52 +0100

    fix error task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,16 +3,13 @@
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
+import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    import urllib.request
-
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body.decode('utf-8')))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
