bdda42f9793297aa8ad79492dd13cb8fa19ba569
Abdeljalil37 <widadpop@gmail.com>
2024-01-08 17:15:31 +0100

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
-"""
+"""Displays the X-Request-Id header variable of a request to a given URL"""
 import sys
 import requests
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
