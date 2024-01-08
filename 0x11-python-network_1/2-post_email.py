d74e6d26f2f448dd8f9fa100cb56b496d7632979
Abdeljalil37 <widadpop@gmail.com>
2024-01-08 14:41:36 +0100

    try fix error task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,16 @@
 - takes email as a parameter
 - displays the body of the response
 """
-import sys
-import urllib.parse
 import urllib.request
+import urllib.parse
+import sys
 
+url = sys.argv[1]
+email = sys.argv[2]
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+    print(body)
