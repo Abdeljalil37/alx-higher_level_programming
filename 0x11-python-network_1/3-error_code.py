90e7b813c8737bf49cb02dc14a2389fab23ef1d7
Abdeljalil37 <widadpop@gmail.com>
2024-01-08 15:01:32 +0100

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
 import sys
-import urllib
+from urllib import error, request
+
+
+if __name__ == "__main__":
+
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read().decode("utf-8")
+            print(body)
+    except error.HTTPError as e:
+        print("Error code:", e.code)
