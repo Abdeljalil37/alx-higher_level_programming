ac9d04fec84fa974c2058671480cb0b2b4b5cf0e
Abdeljalil37 <widadpop@gmail.com>
2024-01-08 15:20:19 +0100

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
+"""
+fetche inside an url and requests model
 """
 import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+resp = requests.get(url)
+print('Body response:')
+print('\t- type:', type(resp.text))
+print('\t- content:',resp.text)
