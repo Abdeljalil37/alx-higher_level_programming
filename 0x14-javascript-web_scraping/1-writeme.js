399923d78b0532d1184dcaf947a4b806a68d4f2e
Abdeljalil37 <widadpop@gmail.com>
2024-01-30 23:22:23 +0100

    fix style task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const outputFile = process.argv[2];
 const stringToWrite = process.argv[3];
 
 fs.writeFile(outputFile, stringToWrite, 'utf8', (err) => {
-    if (err) {
-        console.error(err);
-    }
+  if (err) {
+    console.error(err);
+  }
 });
