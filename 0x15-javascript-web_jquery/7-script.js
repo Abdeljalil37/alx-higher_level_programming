85105c32fabb8800158bfe62d93fc4b41d718976
Abdeljalil37 <widadpop@gmail.com>
2024-02-12 23:43:09 +0100

    Add AJAX request to retrieve character name from SWAPI API

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+        success: function (data) {
+            $('DIV#character').text(data.name);
+        }
+    });
+});
