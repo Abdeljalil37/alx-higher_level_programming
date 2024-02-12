e780b17dc0f0fccc0b3aba15c7186777a733dde0
Abdeljalil37 <widadpop@gmail.com>
2024-02-12 23:51:09 +0100

    Add AJAX request to fetch and display movie titles

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+        type: 'GET',
+        success: function(data){
+            for (let i =0 ; i < data.results.length; i++){
+                $('UL#list_movies').append('<li>' + data.results[i].title + '</li>')
+            }
+        }
+    });
+});
