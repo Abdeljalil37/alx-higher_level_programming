e1c4c15ea2d527d70a94265fad77ffa3916b0e5b
Abdeljalil37 <widadpop@gmail.com>
2024-02-13 00:26:36 +0100

    Update translation functionality

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+$(document).ready(function () {
+    $('INPUT#btn_translate').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://www.fourtonfish.com/hellosalut/?lang=' + $('INPUT#language_code').val(),
+            success: function (data) {
+                $('DIV#hello').text(data.lang);
+            }
+        });
+    });
+});
